fbe18c959bab63875a98e5b00e8b86ac5929c291
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-07-06 22:50:04 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -1,35 +1,38 @@
 class Game
-  attr_accessor :board, :player_1, :player_2
-  attr_reader :turn_count, :token, :winner
 
-WIN_COMBINATIONS = [
-[0,1,2],
-[3,4,5],
-[6,7,8],
-[0,3,6],
-[1,4,7],
-[2,5,8],
-[0,4,8],
-[2,4,6]
-]
+  attr_accessor :board, :player_1, :player_2, :winner, :user_input
 
-  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+  WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6] ]
+
+  def initialize(player_1=Players::Human.new("X"), player_2=Players::Human.new("O"), board=Board.new)
     @player_1 = player_1
     @player_2 = player_2
     @board = board
+    @board.display
   end
 
   def current_player
-    @board.turn_count % 2 == 0 ? player_1 : player_2
+     board.turn_count.odd? ? player_2 : player_1
   end
 
   def won?
-    WIN_COMBINATIONS.detect do |winner|
-      @board.cells[winner[0]] == @board.cells[winner[1]] &&
-      @board.cells[winner[1]] == @board.cells[winner[2]] &&
-      (@board.cells[winner[0]] == "X" || "O")
-    end
-
+    WIN_COMBINATIONS.each do |combination|   #[0,1,2] which is board range (0-8)
+        if @board.cells[combination[0]] == @board.cells[combination[1]] &&
+          @board.cells[combination[1]] == @board.cells[combination[2]] &&
+          @board.taken?(combination[0]+1)
+          #Need to +1, because #taken? rspec test is working off user_input range (1-9)
+          return combination
+        end
+      end
+    return false
   end
 
   def draw?
@@ -37,11 +40,11 @@ WIN_COMBINATIONS = [
   end
 
   def over?
-    won? || draw?
+     (won? || draw?) ? true : false
+     # IF board is not full, game is in progress (FALSE), ELSE, game is over (TRUE)
   end
 
   def winner
-    #binding.pry
     if won?
       combination = won?
       @board.cells[combination[0]] # X or O
@@ -49,25 +52,25 @@ WIN_COMBINATIONS = [
   end
 
   def turn
-       input = current_player.move(board)
-
-       if !board.valid_move?(input)
-           puts "\nInvalid move. Try again."
-           turn
-       else
-           board.update(input, current_player)
-           board.display
-           puts "\n \n"
-       end
-   end
-
-   def play
-     turn until over?
-      if won?
-        puts "Congratulations #{winner}!"
-      elsif draw?
-        puts "Cat's Game!"
-      end
+    puts "Please enter a number 1-9:"
+    @user_input = current_player.move(@board)
+    if @board.valid_move?(@user_input)
+      @board.update(@user_input, current_player)
+    else puts "Please enter a number 1-9:"
+      @board.display
+      turn
     end
+    @board.display
+  end
+
+  def play
+    turn until over?
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
+
 
 end
